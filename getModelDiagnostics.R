b2ec9a62093a00f3c1618267e8e1891730231d85
Waschina <s.waschina@posteo.net>
2019-06-12 15:11:50 +0200

    First commit

---
+++ getModelDiagnostics.R
@@ -0,0 +1,57 @@
+#' Perform a set of sybil modelorg diagnostics tests.
+#' 
+#' @param mod An object of class modelorg.
+#' @param top.x Maximum number of rows displayed in the "Top"-lists.
+#' @return A list for the test results of \code{mod}. List item \code{rxn.info} returns results obtained for individual 
+#'   reactions. Columns include besides identifiers the predcited flux and the reduced costs. List item \code{met.info}
+#'   contains only identfiers at the moment, but will be extented soon.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+getModelDiagnostics <- function(mod, top.x = 10) {
+  require(data.table)
+  require(stringr)
+  
+  sybil::SYBIL_SETTINGS("SOLVER","cplexAPI"); ok <- 1
+  
+  # A - Construct metabolite analysis table
+  met.info <- data.table(id = mod@met_id, name = mod@met_name)
+  
+  # B - Construct reaction analysis table
+  rxn.info <- data.table(id = mod@react_id, name = mod@react_name,
+                         lb = mod@lowbnd, ub = mod@uppbnd)
+  
+  #~~~~~~~~~~~~~~~~~~~~~#
+  # Perform diagnostics #
+  #~~~~~~~~~~~~~~~~~~~~~#
+  
+  # B 1 - MTF flux
+  sol <- sybil::optimizeProb(mod, algorithm = "mtf")
+  rxn.info$flux = sol@fluxdist@fluxes[1:mod@react_num]
+  
+  # B 2 - reduced costs
+  rxn.info$red.costs <- getReducedCosts(mod)
+  
+  
+  #~~~~~~~~~~~~~~~~~~~~~#
+  # Report main results #
+  #~~~~~~~~~~~~~~~~~~~~~#
+  cat("Nutrients, whose infow reached limits (lb):\n\n")
+  print(rxn.info[flux == lb & flux != 0])
+  cat("\n")
+  
+  cat("Top", top.x, "reactions with the lowest reduced costs:\n\n")
+  print(huch$rxn.info[order(red.costs)][1:top.x])
+  cat("\n")
+  
+  cat("Top", top.x, "exchange reactions with the lowest reduced costs:\n\n")
+  print(huch$rxn.info[grepl("^EX", id)][order(red.costs)][1:top.x])
+  cat("\n")
+  
+  cat("Top", top.x, "exchange reactions with flux < 0 (i.e. uptake) and the lowest reduced costs:\n\n")
+  print(huch$rxn.info[grepl("^EX", id) & flux < 0][order(red.costs)][1:top.x])
+  cat("\n")
+  
+  return(list(met.info = met.info,
+              rxn.info = rxn.info))
+}
